89a98de2a80405b36ff9647d2bb92d47a0426836
Maicol Bentancor <maicol.bentancor@gmail.com>
2016-10-27 16:07:55 -0300

    Added carriewave

---
+++ config/initializers/carrierwave.rb
@@ -0,0 +1,14 @@
+CarrierWave.configure do |config|
+  if Rails.env.test? or Rails.env.cucumber?
+    config.storage = :file
+    config.enable_processing = false
+  else
+    config.storage = :fog
+    config.fog_credentials = {
+      provider: 'AWS',
+      aws_access_key_id: ENV['AWS_ACCESS_KEY_ID'],
+      aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY']
+    }
+    config.fog_directory  = ENV['S3_BUCKET_NAME']
+  end
+end
