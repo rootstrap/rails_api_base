ee5463004915077c7df56386b8820fafd0d042a0
Diego Barral <diego@rootstrap.com>
2017-09-11 15:12:48 -0300

    Fix for the string not matched issue

--- spec/requests/api/v1/sessions/create_spec.rb
+++ spec/requests/api/v1/sessions/create_spec.rb
@@ -2,7 +2,23 @@ require 'rails_helper'
 
 describe 'POST api/v1/users/sign_in', type: :request do
   let(:password) { 'password' }
-  let(:user)     { create(:user, password: password) }
+  let(:token) do
+    { '70crCAAYmNP1xLkKKM09zA' =>
+      {
+        'token' => '$2a$10$mSeRnpVMaaegCpn3AhORGe5wajFhgMoBjGIrMwq4Qq2mP6f/OHu1y',
+        'expiry' => 153_574_356_4
+      }
+    }
+  end
+  let(:token_as_string) do
+    "{
+      \"70crCAAYmNP1xLkKKM09zA\": {
+        \"token\":\"$2a$10$mSeRnpVMaaegCpn3AhORGe5wajFhgMoBjGIrMwq4Qq2mP6f/OHu1y\",
+        \"expiry\":1535743564
+      }
+    }"
+  end
+  let(:user)     { create(:user, password: password, tokens: token) }
 
   context 'with correct params' do
     before do
@@ -52,6 +68,40 @@ describe 'POST api/v1/users/sign_in', type: :request do
         errors: ['Invalid login credentials. Please try again.']
       }.with_indifferent_access
       expect(json).to eq(expected_response)
+    end
+  end
+
+  context 'with stringified tokens' do
+    before do
+      user.update_column(:tokens, token_as_string)
+      params = {
+        user:
+          {
+            email: user.email,
+            password: password
+          }
+      }
+      post new_user_session_path, params: params, as: :json
+    end
+
+    it 'returns success' do
+      expect(response).to be_success
+    end
+
+    it 'returns the user' do
+      expect(json[:user][:id]).to eq(user.id)
+      expect(json[:user][:email]).to eq(user.email)
+      expect(json[:user][:username]).to eq(user.username)
+      expect(json[:user][:uid]).to eq(user.uid)
+      expect(json[:user][:provider]).to eq('email')
+      expect(json[:user][:first_name]).to eq(user.first_name)
+      expect(json[:user][:last_name]).to eq(user.last_name)
+    end
+
+    it 'returns a valid client and access token' do
+      token = response.header['access-token']
+      client = response.header['client']
+      expect(user.reload.valid_token?(token, client)).to be_truthy
     end
   end
 end
