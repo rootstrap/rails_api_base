6863e8ac7d961df822c239a6e843387c50882273
Maicol Bentancor <maicol.bentancor@gmail.com>
2017-10-20 11:35:55 -0300

    Move strings to locals

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -11,9 +11,9 @@ module Api
         @resource = User.from_social_provider 'facebook', user_params
         custom_sign_in
       rescue Koala::Facebook::AuthenticationError
-        render json: { error: 'Not Authorized' }, status: :forbidden
+        render json: { error: I18n.t('api.facebook.not_authorized') }, status: :forbidden
       rescue ActiveRecord::RecordNotUnique
-        render json: { error: 'User already registered with email/password' }, status: :bad_request
+        render json: { error: I18n.t('api.facebook.already_registerd') }, status: :bad_request
       end
 
       def resource_params
