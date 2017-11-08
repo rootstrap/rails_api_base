46bda6d5ae8a40f202b005973132cc48e90bfc1b
Maicol Bentancor <maicol.bentancor@gmail.com>
2017-11-08 15:04:51 -0300

    Change token lifespan

--- config/initializers/devise_token_auth.rb
+++ config/initializers/devise_token_auth.rb
@@ -2,7 +2,7 @@ DeviseTokenAuth.setup do |config|
   config.default_confirm_success_url = '/'
   config.default_password_reset_url = ENV['PASSWORD_RESET_URL']
   config.enable_standard_devise_support = true
-  config.token_lifespan = 1.year
+  config.token_lifespan = 2.years
   config.batch_request_buffer_throttle = 10.seconds
   config.change_headers_on_each_request = false
 end
