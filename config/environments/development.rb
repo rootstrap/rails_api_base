c65a8f654055607bdc84b12bca46966cb41060ef
Santiago Bartesaghi <santiago.bartesaghi@rootstrap.com>
2018-11-02 11:31:44 -0300

    Install Active Storage

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -41,6 +41,9 @@ Rails.application.configure do
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
 
+  # Store files locally.
+  config.active_storage.service = :local
+
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
