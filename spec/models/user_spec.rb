64e8755df7fc845b1cd1b01a1256f1f315b5d3d7
gonzaM <gonzalo@rootstrap.com>
2018-11-23 09:49:04 -0300

    Convert to rspec 3 some rspec 2 examples

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -34,12 +34,12 @@ require 'rails_helper'
 describe User do
   describe 'validations' do
     subject { build :user }
-    it { should validate_uniqueness_of(:uid).scoped_to(:provider) }
+    it { is_expected.to validate_uniqueness_of(:uid).scoped_to(:provider) }
 
     context 'when was created with regular login' do
       subject { build :user }
-      it { should validate_uniqueness_of(:email).case_insensitive.scoped_to(:provider) }
-      it { should validate_presence_of(:email) }
+      it { is_expected.to validate_uniqueness_of(:email).case_insensitive.scoped_to(:provider) }
+      it { is_expected.to validate_presence_of(:email) }
     end
   end
 
