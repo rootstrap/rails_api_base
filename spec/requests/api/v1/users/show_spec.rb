e2277ecb455812d9482b391b841c331d0e6f0653
Santiago Bartesaghi <santiago.bartesaghi@rootstrap.com>
2019-01-24 12:57:37 -0300

    By default a user can only see/update it's data

--- spec/requests/api/v1/users/show_spec.rb
+++ spec/requests/api/v1/users/show_spec.rb
@@ -2,17 +2,16 @@ require 'rails_helper'
 
 describe 'GET api/v1/users/:id', type: :request do
   let(:user) { create(:user) }
-  let(:another_user) { create :user }
 
   it 'returns success' do
-    get api_v1_user_path(id: another_user.id), headers: auth_headers, as: :json
+    get api_v1_user_path, headers: auth_headers, as: :json
     expect(response).to have_http_status(:success)
   end
 
-  it 'returns user\'s data' do
-    get api_v1_user_path(id: another_user.id), headers: auth_headers, as: :json
+  it "returns the logged in user's data" do
+    get api_v1_user_path, headers: auth_headers, as: :json
 
-    expect(json[:user][:id]).to eq another_user.id
-    expect(json[:user][:first_name]).to eq another_user.first_name
+    expect(json[:user][:id]).to eq user.id
+    expect(json[:user][:first_name]).to eq user.first_name
   end
 end
