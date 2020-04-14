e10a08e107635f14526dd83b59ffc8c17cc0e2aa
fedeagripa <federicogagripa@gmail.com>
2020-04-14 15:18:47 -0300

    add user policy

--- app/controllers/api/v1/settings_controller.rb
+++ app/controllers/api/v1/settings_controller.rb
@@ -2,6 +2,7 @@ module Api
   module V1
     class SettingsController < Api::V1::ApiController
       skip_before_action :authenticate_user!
+      skip_after_action :verify_authorized, :verify_policy_scoped
 
       def must_update
         current_version = Gem::Version.new(params[:device_version])
---
+++ app/policies/user_policy.rb
@@ -0,0 +1,13 @@
+class UserPolicy < ApplicationPolicy
+  def show?
+    true
+  end
+
+  def profile?
+    update?
+  end
+
+  def update?
+    user.id == record.id
+  end
+end
