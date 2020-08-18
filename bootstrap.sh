c8e8976eedd4915723e01f2eeaa1e732372020a8
Bruno Vezoli <c-bruno.vezoli@masterclass.com>
2020-08-18 11:25:20 -0300

    Fix secrets duplication

--- bootstrap.sh
+++ bootstrap.sh
@@ -16,9 +16,8 @@ bundle install
 #install node packages
 yarn
 
-# copy secrets from the example adding a new application secret
-application_secret=$(bundle exec rake secret)
-sed "s/SECRET_KEY_BASE:/SECRET_KEY_BASE: ${application_secret}/g" config/application.yml.example > config/application.yml
+# generate an application.yml from the example file
+cp config/application.yml.example config/application.yml
 
 # copy database configuration and change the project name with the given one
 sed "s/sample_project/${project_name}/g" config/database.yml.example > config/database.yml
--- config/initializers/figaro.rb
+++ config/initializers/figaro.rb
@@ -1,9 +1,1 @@
-# Put here the variables used by all the environments
-variables = %w[SERVER_URL PASSWORD_RESET_URL]
-
-unless Rails.env.test?
-  # Variables not used by the test environment
-  variables += %w[SECRET_KEY_BASE]
-end
-
-Figaro.require_keys(variables)
+Figaro.require_keys(%w[SERVER_URL PASSWORD_RESET_URL])
