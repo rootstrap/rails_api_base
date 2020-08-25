bd8506b5d212146ce0c622e97e4e699a22162e68
Juan Ferrari <juan.ferrari@rootstrap.com>
2020-08-25 16:22:33 -0300

    Add some tests

--- spec/policies/application_policy_spec.rb
+++ spec/policies/application_policy_spec.rb
@@ -2,10 +2,41 @@ describe ApplicationPolicy do
   subject { described_class }
 
   permissions :update?, :index?, :show?, :create?, :new?, :edit?, :destroy? do
-    let(:user) { create(:user) }
+    context 'when is an user' do
+      let!(:user) { create(:user) }
 
-    it 'denies access' do
-      expect(subject).not_to permit
+      it 'denies access' do
+        expect(subject).not_to permit(user, user)
+      end
+    end
+
+    context 'when is an admin' do
+      let!(:admin) { create(:admin_user) }
+
+      it 'permits access' do
+        expect(subject).to permit(admin, admin)
+      end
+    end
+  end
+
+  describe 'scope' do
+    let(:mock_model) { OpenStruct.new(all: true) }
+    subject { ApplicationPolicy::Scope.new(user, mock_model).resolve }
+
+    context 'when is an user' do
+      let!(:user) { create(:user) }
+
+      it 'hides all models' do
+        expect(subject).to be_nil
+      end
+    end
+
+    context 'when is an admin' do
+      let!(:user) { create(:admin_user) }
+
+      it 'shows all models' do
+        expect(subject).to_not be_nil
+      end
     end
   end
 end
--- spec/requests/api/v1/users/show_spec.rb
+++ spec/requests/api/v1/users/show_spec.rb
@@ -1,15 +1,27 @@
 describe 'GET api/v1/users/:id', type: :request do
   let(:user) { create(:user) }
+  subject { get api_v1_user_path, headers: auth_headers, as: :json }
 
   it 'returns success' do
-    get api_v1_user_path, headers: auth_headers, as: :json
+    subject
     expect(response).to have_http_status(:success)
   end
 
   it "returns the logged in user's data" do
-    get api_v1_user_path, headers: auth_headers, as: :json
+    subject
 
-    expect(json[:user][:id]).to eq user.id
-    expect(json[:user][:first_name]).to eq user.first_name
+    expect(json[:user][:id]).to eq(user.id)
+    expect(json[:user][:first_name]).to eq(user.first_name)
+  end
+
+  context 'when record is not found' do
+    it 'returns status 404 not found' do
+      allow_any_instance_of(Api::V1::UsersController).to receive(
+        :current_user
+      ).and_raise(ActiveRecord::RecordNotFound)
+      subject
+
+      expect(response).to have_http_status(:not_found)
+    end
   end
 end
