56317457d5f70a655b4a0b01c32a75e371007a3c
Bruno Vezoli <bruno.vezoli@rootstrap.com>
2020-08-26 12:54:10 -0300

    Add bootstrap script (#243)

---
+++ bootstrap.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+set -exu
+
+die () {
+  echo >&2 "$@"
+  exit 1
+}
+
+[ "$#" -eq 1 ] || die "1 argument required, $# provided. Please add the project name"
+
+project_name=$1
+
+# install ruby gems
+bundle install
+
+#install node packages
+yarn
+
+# generate an application.yml from the example file
+cp config/application.yml.example config/application.yml
+
+# copy database configuration and change the project name with the given one
+sed "s/sample_project/${project_name}/g" config/database.yml.example > config/database.yml
+
+# setup database
+bundle exec rails db:setup
