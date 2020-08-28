dd2d5831b8dc92b9f806c9771ef9074b60c07392
Juan Ferrari <juan.ferrari@rootstrap.com>
2020-08-28 11:16:29 -0300

    Update spec/policies/application_policy_spec.rb
    Co-authored-by: Bruno Vezoli <bruno.vezoli@rootstrap.com>

--- spec/policies/application_policy_spec.rb
+++ spec/policies/application_policy_spec.rb
@@ -3,7 +3,7 @@ describe ApplicationPolicy do
 
   permissions :update?, :index?, :show?, :create?, :new?, :edit?, :destroy? do
     context 'when is an user' do
-      let!(:user) { create(:user) }
+      let(:user) { build(:user) }
 
       it 'denies access' do
         expect(subject).not_to permit(user, user)
