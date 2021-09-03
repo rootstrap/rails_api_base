8a1dd57f94f9f5831bb7fe5febcc477452395209
Diego Suárez <diego.suarez@rootstrap.com>
2021-09-03 12:58:50 -0300

    disable forgery protection for api calls on Devise controllers

--- app/controllers/api/v1/passwords_controller.rb
+++ app/controllers/api/v1/passwords_controller.rb
@@ -1,7 +1,7 @@
 module Api
   module V1
     class PasswordsController < DeviseTokenAuth::PasswordsController
-      protect_from_forgery with: :exception
+      protect_from_forgery with: :exception, unless: :json_request?
       include Api::Concerns::ActAsApiRequest
       skip_before_action :check_json_request, on: :edit
     end
