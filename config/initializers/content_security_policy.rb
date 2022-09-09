4b776ba43fb58263ad52c2420bb3f1e3255108c3
Daniel Serrano <daniel.serrano@koombea.com>
2022-09-09 14:43:58 -0500

    remove non rails 7 comments from content_security_policy.rb

--- config/initializers/content_security_policy.rb
+++ config/initializers/content_security_policy.rb
@@ -24,10 +24,3 @@
 #   # config.content_security_policy_report_only = true
 # end
 
-# Set the nonce only to specific directives
-# Rails.application.config.content_security_policy_nonce_directives = %w(script-src)
-
-# Report CSP violations to a specified URI
-# For further information see the following documentation:
-# https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Content-Security-Policy-Report-Only
-# Rails.application.config.content_security_policy_report_only = true
