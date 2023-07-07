e18f3103376c42464c98f08970fdbe9423ffd850
Mauricio Rodríguez <35469032+mrodriguez90@users.noreply.github.com>
2023-07-07 17:00:13 -0300

    Set gem versions, use devise authenticate instead of a constraint

--- config/initializers/flipper.rb
+++
@@ -1,6 +0,0 @@
-class CanAccessFlipperUI
-  def self.matches?(request)
-    current_user = request.env['warden'].user
-    current_user.present? && current_user.is_a?(AdminUser)
-  end
-end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
-  constraints CanAccessFlipperUI do
-    mount Flipper::UI.app(Flipper) => '/flipper'
+  namespace :admin do
+    authenticate(:admin_user) do
+      mount Flipper::UI.app(Flipper) => '/feature-flags'
+    end
   end
   ExceptionHunter.routes(self)
   mount_devise_token_auth_for 'User', at: '/api/v1/users', controllers: {
