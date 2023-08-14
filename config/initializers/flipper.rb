fe1f19e3b9a65b14a14a42a1755013e371013336
Mauricio Rodríguez <35469032+mrodriguez90@users.noreply.github.com>
2023-08-14 13:17:21 -0300

    Fix LineLength cop

--- config/initializers/flipper.rb
+++ config/initializers/flipper.rb
@@ -1,7 +1,9 @@
 FEATURES_YML_PATH = 'config/feature-flags.yml'.freeze
 Flipper::UI.configure do |config|
   config.descriptions_source = lambda { |_keys|
-    YAML.load_file(FEATURES_YML_PATH, fallback: {}).transform_values { |value| value['description'] }
+    YAML.load_file(FEATURES_YML_PATH, fallback: {}).transform_values do |value|
+      value['description']
+    end
   }
   config.show_feature_description_in_list = true
 end
