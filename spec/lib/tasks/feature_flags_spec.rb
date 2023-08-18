ba079d704b42471b1462bba5b5f764eff8a7a422
Mauricio Rodríguez <35469032+mrodriguez90@users.noreply.github.com>
2023-08-18 13:53:39 -0300

    Add test for feature_flags.rake

---
+++ spec/lib/tasks/feature_flags_spec.rb
@@ -0,0 +1,32 @@
+require 'rake'
+
+RSpec.describe 'Feature Flags', :rake do
+  before(:all) do
+    task_defined = Rake::Task.task_defined?('feature_flags:initialize')
+    Rails.application.load_tasks unless task_defined
+  end
+
+  describe 'initialize' do
+    let(:feature_flags) do
+      {
+        'test_flag' => {
+          'description' => 'Test description',
+          'owner' => 'Test owner',
+          'removal_ticket' => 'Test ticket'
+        }
+      }
+    end
+
+    before do
+      allow(YAML).to receive(:load_file).and_return(feature_flags)
+    end
+
+    it 'registers the feature flags from config/feature-flags.yml' do
+      expect(Flipper.features.count).to eq 0
+      Rake::Task['feature_flags:initialize'].invoke
+      expect(Flipper.features.count).to eq 1
+      expect(Flipper.features.first.name).to eq 'test_flag'
+      expect(Flipper.features.first.state).to eq :off
+    end
+  end
+end
