63bc3660aa7f22111c4c3c49dedd904e1a3aa34d
Santiago Bartesaghi <santib@hey.com>
2023-09-07 17:10:00 -0300

    Fix Pundit setup (#440)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,10 +6,10 @@ class ApplicationController < ActionController::Base
 
   after_action :verify_authorized,
                except: :index,
-               unless: -> { :devise_controller? || :active_admin_controller? }
+               unless: -> { devise_controller? || active_admin_controller? }
   after_action :verify_policy_scoped,
                only: :index,
-               unless: -> { :devise_controller? || :active_admin_controller? }
+               unless: -> { devise_controller? || active_admin_controller? }
   # Prevent CSRF attacks by raising an exception.
   protect_from_forgery with: :exception
 
