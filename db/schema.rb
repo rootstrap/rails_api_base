fcf60011ae668e4b30ad5060b1630d6b3a842d3c
Guillermo Aguirre <guillermoaguirre1@gmail.com>
2023-09-08 17:48:45 -0300

    Update db/schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,8 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_09_08_202728) do
+ActiveRecord::Schema[7.0].define(version: 2023_07_04_182448) do
   # These are extensions that must be enabled in order to support this database
-  enable_extension "pg_trgm"
   enable_extension "plpgsql"
 
   create_table "active_storage_attachments", force: :cascade do |t|
