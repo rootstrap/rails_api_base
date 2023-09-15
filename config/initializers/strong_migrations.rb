0d968fb340bf97c2feb1cc45203fc5bddf63254d
brunoparma88 <53717512+brunoparma88@users.noreply.github.com>
2023-09-15 16:01:47 -0300

    fix linters

--- config/initializers/strong_migrations.rb
+++ config/initializers/strong_migrations.rb
@@ -1,7 +1,9 @@
-# To mark migrations as safe that were created before installing this gem
-StrongMigrations.start_after = 20300101000000  # Disabled By default #2030/01/01
+# frozen_string_literal: true
 
-#Analyze tables automatically (to update planner statistics) after an index is added
+# To mark migrations as safe that were created before installing this gem
+StrongMigrations.start_after = 20_300_101_000_000 # Disabled By default #2030/01/01
+
+# Analyze tables automatically (to update planner statistics) after an index is added
 StrongMigrations.auto_analyze = true
 
 StrongMigrations.lock_timeout = 10.seconds
