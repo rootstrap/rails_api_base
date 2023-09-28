7e1a5e7056579fba2fcda62f93dce984fe827d13
Guillermo Aguirre <guillermoaguirre1@gmail.com>
2023-09-28 16:31:25 -0300

    Freeze test data for docs

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -34,6 +34,7 @@ RSpec.configure do |config|
   config.include Devise::Test::ControllerHelpers, type: :controller
   config.include Devise::Test::IntegrationHelpers, type: :request
   config.include ActiveJob::TestHelper
+  config.include ActiveSupport::Testing::TimeHelpers
   config.use_transactional_fixtures = true
   config.infer_spec_type_from_file_location!
 
