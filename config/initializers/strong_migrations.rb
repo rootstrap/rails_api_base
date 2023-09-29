115b21867443f1d2b85b1930615f1f0912b2e6cc
brunoparma88 <53717512+brunoparma88@users.noreply.github.com>
2023-09-29 08:39:29 -0300

    update gemfile

--- config/initializers/strong_migrations.rb
+++ config/initializers/strong_migrations.rb
@@ -1,10 +1,9 @@
 # frozen_string_literal: true
 
 # To mark migrations as safe that were created before installing this gem
-# StrongMigrations.start_after = 20250101000000 # Disabled By default #2030/01/01
+StrongMigrations.start_after = 20300101000000 # Disabled By default #2030/01/01
 
 # Analyze tables automatically (to update planner statistics) after an index is added
 StrongMigrations.auto_analyze = true
-
 StrongMigrations.lock_timeout = 10.seconds
 StrongMigrations.statement_timeout = 1.hour
