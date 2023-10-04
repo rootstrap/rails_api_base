81ac1fe89750c35e001933d142378e80b326de42
Ignacio Perez <ignacio.perez@rootstrap.com>
2023-10-04 17:36:35 -0300

    Small changes

--- esbuild.config.mjs
+++ esbuild.config.mjs
@@ -3,13 +3,13 @@ import * as esbuild from 'esbuild'
 const config = {
   entryPoints: ['app/assets/javascripts/active_admin.js'],
   bundle: true,
-  sourcemap: true,
-  minify: process.env.RAILS_ENV == "production",
+  sourcemap: process.env.RAILS_ENV != 'production',
+  minify: process.env.RAILS_ENV == 'production',
   outdir: 'app/assets/builds',
   publicPath: '/assets'
 }
 
-if (process.argv.includes("--watch")) {
+if (process.argv.includes('--watch')) {
   let context = await esbuild.context({...config, logLevel: 'info'})
   context.watch()
 } else {
