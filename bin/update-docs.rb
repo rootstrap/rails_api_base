d7df38756f81b8dcd1645caa3dc71896e22f6235
Guillermo Aguirre <guillermoaguirre1@gmail.com>
2023-10-06 12:29:55 -0300

    Clear whitespaces from doc file

--- bin/update-docs.rb
+++ bin/update-docs.rb
@@ -3,19 +3,30 @@
 require 'yaml'
 require 'active_support/core_ext/hash/deep_merge'
 
+# sed command implementation is different for GNU and macOS
+def sed_i(file)
+  if /darwin/.match?(RUBY_PLATFORM)
+	  system "sed -i '' -E 's/[ '$'\t'']+$//' #{file}"
+  else
+    system "sed -i 's/[ \t]*$//' #{file}"
+  end
+end
+
 if ENV['PARALLEL_TESTS_CONCURRENCY']
   begin
     system "OPENAPI=1 bundle exec parallel_rspec -n #{ENV['PARALLEL_TESTS_CONCURRENCY']} spec/requests/api/ -o '--seed 1993'"
-    file = YAML.safe_load(File.read('./doc/openapi.yaml'))
+    path = './doc/openapi.yaml'
+    file = YAML.safe_load(File.read(path))
     (2..ENV['PARALLEL_TESTS_CONCURRENCY'].to_i).each do |number|
-      path = "./doc/openapi#{number}.yaml"
-      file.deep_merge!(YAML.safe_load(File.read(path)))
+      file.deep_merge!(YAML.safe_load(File.read("./doc/openapi#{number}.yaml")))
     end
-    File.write('./doc/openapi.yaml', YAML.dump(file))
+    File.write(path, YAML.dump(file))
+    # Remove trailing whitespaces from doc file
+    sed_i(path)
   rescue Errno::ENOENT => exception
     puts exception
   ensure
-    exec 'git clean -f doc/openapi?.yaml'
+    `git clean -f ./doc/openapi?.yaml`
   end
 else
   exec 'OPENAPI=1 bundle exec rspec spec/requests/api/ --seed 1993'
