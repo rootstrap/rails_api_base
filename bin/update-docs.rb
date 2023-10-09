dd456c33cbfccfe18a3553258e37e26f1f511670
Guillermo Aguirre <guillermoaguirre1@gmail.com>
2023-10-09 17:54:54 -0300

    Stop freezing time in specs, set dates with regex in doc file

--- bin/update-docs.rb
+++ bin/update-docs.rb
@@ -3,12 +3,22 @@
 require 'yaml'
 require 'active_support/core_ext/hash/deep_merge'
 
+PATH = './doc/openapi.yaml'.freeze
+
 # sed command implementation is different for GNU and macOS
-def sed_i(file)
+def sed_i(regex)
   if /darwin/.match?(RUBY_PLATFORM)
-    system "sed -i '' -E 's/[ '$'\t'']+$//' #{file}"
+    system "sed -i '' -E 's/#{regex}/g' #{PATH}"
   else
-    system "sed -i 's/[ \t]*$//' #{file}"
+    system "sed -i 's/#{regex}/g' #{PATH}"
+  end
+end
+
+def remove_white_spaces
+  if /darwin/.match?(RUBY_PLATFORM)
+    sed_i("[ '$'\t'']+$/")
+  else
+    sed_i("[ \t]*$/")
   end
 end
 
@@ -18,16 +28,16 @@ else
   begin
     concurrency = ENV.fetch('PARALLEL_TESTS_CONCURRENCY', 8)
     system "OPENAPI=1 bundle exec parallel_rspec -n #{concurrency} spec/requests/api/ -o '--seed 1993'"
-    path = './doc/openapi.yaml'
-    file = YAML.safe_load(File.read(path))
+    file = YAML.safe_load(File.read(PATH))
     (2..concurrency.to_i).each do |number|
       file.deep_merge!(YAML.safe_load(File.read("./doc/openapi#{number}.yaml")))
     end
     # Sort endpoints alphabetically
     file['paths'] = file['paths'].sort.to_h
-    File.write(path, YAML.dump(file))
-    # Remove trailing whitespaces from doc file
-    sed_i(path)
+    # Freeze all created_at & updated_at dates to 1/1/2023
+    file = YAML.dump(file).gsub(/(created_at|updated_at): '.*'/, "\0: '2023-01-01T00:00:00.000Z'")
+    File.write(PATH, file)
+    remove_white_spaces
   rescue Errno::ENOENT => exception
     puts exception
   ensure
