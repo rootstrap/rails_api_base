aad8039d680a166f0abca29b7b63fc50894b2dff
Guillermo Aguirre <guillermoaguirre1@gmail.com>
2023-10-09 11:40:07 -0300

    Add comment to initializer

--- config/initializers/rspec_openapi.rb
+++ config/initializers/rspec_openapi.rb
@@ -8,4 +8,5 @@ RSpec::OpenAPI.request_headers = %w[access-token uid client]
 # Set response `headers` - generate parameters with headers for a response
 RSpec::OpenAPI.response_headers = %w[access-token expiry token-type uid client]
 
+# Support generating the docs when running specs with `parallel_tests`
 RSpec::OpenAPI.path = ->(_) { "doc/openapi#{ENV.fetch('TEST_ENV_NUMBER')}.yaml" }
