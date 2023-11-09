a869dc3ac79fb797f69fbe83830a50517512e100
Samir Tapiero <samir.tapiero@rootstrap.com>
2023-11-09 11:11:51 -0500

    Add SwaggerUi to replace Apiary, expose api-doc and add basic-auth

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  mount Rswag::Ui::Engine => '/api-docs'
+  mount Rswag::Api::Engine => '/api-docs'
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   namespace :admin do
