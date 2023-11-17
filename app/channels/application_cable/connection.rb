43636ee0fb81dd75b7da5ec60dcc8014ee681ad8
enzofab91 <enzofab91@gmail.com>
2023-11-17 19:33:57 -0300

    Add pretender gem and basic configuration

--- app/channels/application_cable/connection.rb
+++ app/channels/application_cable/connection.rb
@@ -2,5 +2,7 @@
 
 module ApplicationCable
   class Connection < ActionCable::Connection::Base
+    identified_by :current_user, :true_user
+    impersonates :user
   end
 end
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
+  impersonates :user
+
   include Pundit::Authorization
   include ActiveStorage::SetCurrent
 
