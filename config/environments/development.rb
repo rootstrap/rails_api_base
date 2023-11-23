a4af2c7675bd6bdb20de284e47764683e30abc60
Santiago Bartesaghi <sbartesaghi@hotmail.com>
2023-11-23 15:00:02 -0300

    Revert api_only config

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -85,6 +85,4 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
-
-  config.debug_exception_response_format = :default
 end
