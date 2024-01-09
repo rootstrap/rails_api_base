f472622aef11f73d7d7252bc94a948d1e023ba38
Santiago Bartesaghi <sbartesaghi@hotmail.com>
2024-01-09 18:17:01 -0300

    Refactor

--- bin/fullstack.rb
+++ bin/fullstack.rb
@@ -1,15 +1,15 @@
 # Add gems
-insert_into_file 'Gemfile', after: /gem 'rails'.*\n\n/ do <<~EOF
-  gem 'cssbundling-rails', '~> 1.3'
-  gem 'stimulus-rails', '~> 1.3'
-  gem 'turbo-rails', '2.0.0.pre.beta.2'
-  gem 'view_component', '~> 3.7.0'
-  EOF
+insert_into_file 'Gemfile', after: /gem 'rails'.*\n\n/ do <<-EOF
+gem 'cssbundling-rails', '~> 1.3'
+gem 'stimulus-rails', '~> 1.3'
+gem 'turbo-rails', '2.0.0.pre.beta.2'
+gem 'view_component', '~> 3.7.0'
+EOF
 end
 
-insert_into_file 'Gemfile', after: "group :development do\n" do <<~EOF
-  gem 'lookbook', '~> 2.1'
-  EOF
+insert_into_file 'Gemfile', after: "group :development do\n" do <<-EOF
+gem 'lookbook', '~> 2.1'
+EOF
 end
 
 Bundler.with_unbundled_env { run 'bundle install' }
@@ -24,39 +24,39 @@ run './bin/rails css:install:tailwind'
 
 # Update application layout
 gsub_file 'app/views/layouts/application.html.erb', "    <%= stylesheet_link_tag \"application\" %>\n", ''
-insert_into_file 'app/views/layouts/application.html.erb', before: /\s*<\/head>/ do <<~EOF
-  \n    <%= javascript_include_tag \"application\", \"data-turbo-track\": \"reload\", defer: true %>
-  EOF
+insert_into_file 'app/views/layouts/application.html.erb', before: /\s*<\/head>/ do <<-EOF
+\n    <%= javascript_include_tag \"application\", \"data-turbo-track\": \"reload\", defer: true %>
+EOF
 end
 
 # Application JS entrypoint
-add_file 'app/javascript/application.js' do <<~EOF
-  // Entry point for the build script in your package.json
-  import "@hotwired/turbo-rails"
-  import "./controllers"
-  EOF
+add_file 'app/javascript/application.js' do <<-EOF
+// Entry point for the build script in your package.json
+import "@hotwired/turbo-rails"
+import "./controllers"
+EOF
 end
 
 # StimulusJS
-add_file 'app/javascript/controllers/application.js' do <<~EOF
-  import { Application } from "@hotwired/stimulus"
+add_file 'app/javascript/controllers/application.js' do <<-EOF
+import { Application } from "@hotwired/stimulus"
 
-  const application = Application.start()
+const application = Application.start()
 
-  // Configure Stimulus development experience
-  application.debug = false
-  window.Stimulus   = application
+// Configure Stimulus development experience
+application.debug = false
+window.Stimulus   = application
 
-  export { application }
-  EOF
+export { application }
+EOF
 end
 
-add_file 'app/javascript/controllers/index.js' do <<~EOF
-  import { application } from "./application"
+add_file 'app/javascript/controllers/index.js' do <<-EOF
+import { application } from "./application"
 
-  // Import app/components/index.js
-  import "../../components"
-  EOF
+// Import app/components/index.js
+import "../../components"
+EOF
 end
 
 insert_into_file 'esbuild.config.mjs', after: "import * as esbuild from 'esbuild'\n" do <<-EOF
@@ -70,24 +70,24 @@ EOF
 end
 
 # ViewComponent config
-add_file 'app/components/index.js' do <<~EOF
-  import { application } from "../javascript/controllers/application"
-  import controllers from "./**/*_controller.js"
+add_file 'app/components/index.js' do <<-EOF
+import { application } from "../javascript/controllers/application"
+import controllers from "./**/*_controller.js"
 
-  controllers.forEach((controller) => {
-    application.register(controller.name, controller.module.default)
-  })
-  EOF
+controllers.forEach((controller) => {
+  application.register(controller.name, controller.module.default)
+})
+EOF
 end
 
-insert_into_file 'config/environments/development.rb', before: /^end$/ do <<~EOF
+insert_into_file 'config/environments/development.rb', before: /^end$/ do <<-EOF
 
-    # ViewComponent Previews
-    config.view_component.preview_paths << "\#{Rails.root}/spec/components/previews"
-  EOF
+  # ViewComponent Previews
+  config.view_component.preview_paths << "\#{Rails.root}/spec/components/previews"
+EOF
 end
 
-insert_into_file 'config/routes.rb', before: /^end$/ do <<~EOF
+insert_into_file 'config/routes.rb', before: /^end$/ do <<-EOF
     mount Lookbook::Engine, at: '/lookbook' if Rails.env.development?
   EOF
 end
