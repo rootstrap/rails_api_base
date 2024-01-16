6c412d1ac873aecfc8e35b0ae0a8ce97ce8e6d8d
Santiago Bartesaghi <santib@hey.com>
2024-01-16 19:40:33 -0300

    Update config/routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  scope format: 'json' do
+  scope format: :json do
     mount_devise_token_auth_for 'User', at: '/api/v1/users', controllers: {
       registrations: 'api/v1/registrations',
       sessions: 'api/v1/sessions',
