4a4ae1906547a001c8b6749f0b43142cbff98912
Bjoern <bjoern.fc@gmail.com>
2024-01-16 13:54:00 -0800

    Ensuring consistency of API routes. Need to force JSON scope to prevent devise routes from defaulting the request format to */*

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  mount_devise_token_auth_for 'User', at: '/api/v1/users', controllers: {
-    registrations: 'api/v1/registrations',
-    sessions: 'api/v1/sessions',
-    passwords: 'api/v1/passwords'
-  }
+  scope format: 'json' do
+    mount_devise_token_auth_for 'User', at: '/api/v1/users', controllers: {
+      registrations: 'api/v1/registrations',
+      sessions: 'api/v1/sessions',
+      passwords: 'api/v1/passwords'
+    }
+  end
 
   namespace :api do
     namespace :v1, defaults: { format: :json } do
