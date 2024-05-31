8bf67bbb123f480f799d0b5d252ec5eeacade7fa
Andres Garcia <andres@rootstrap.com>
2024-05-31 16:26:05 -0300

    Add require

--- lib/active_storage/service/disk_with_host_service.rb
+++ lib/active_storage/service/disk_with_host_service.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+require 'active_storage/service/disk_service'
+
 module ActiveStorage
   class Service
     class DiskWithHostService < ActiveStorage::Service::DiskService
