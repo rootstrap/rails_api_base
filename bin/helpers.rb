738be7e5d802690106179c564b2eabbf697a04dd
Santiago Bartesaghi <sbartesaghi@hotmail.com>
2024-06-12 11:04:37 -0300

    Fix Docker usage

--- bin/helpers.rb
+++ bin/helpers.rb
@@ -7,9 +7,13 @@ rescue StandardError, LoadError => e
 end
 
 def running_with_docker?
-  ENV['DOCKER_ENABLED'] == 'true' && docker_compose_installed?
+  ENV['DOCKER_ENABLED'] == 'true' && docker_compose_installed? && docker_running?
 end
 
 def docker_compose_installed?
   system('which docker-compose > /dev/null 2>&1')
+end
+
+def docker_running?
+  system('docker ps > /dev/null 2>&1')
 end
