97d69c5f36e5b5f7d99ef4220e73aa421852b851
Samir Tapiero <samir.tapiero@rootstrap.com>
2024-10-16 14:35:06 -0500

    Refactor impersonation action to prevent the redirect when admin session expired

--- app/policies/admin/user_policy.rb
+++ app/policies/admin/user_policy.rb
@@ -1,5 +1,9 @@
 # frozen_string_literal: true
 
 module Admin
-  class UserPolicy < Admin::ApplicationPolicy; end
+  class UserPolicy < Admin::ApplicationPolicy
+    def impersonate?
+      create? && Flipper.enabled?(:impersonation_tool)
+    end
+  end
 end
