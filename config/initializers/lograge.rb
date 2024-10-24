cf77885ac3d6dbfd088aa28760520e2c04a9d9d8
Santiago Bartesaghi <santib@hey.com>
2024-10-24 16:08:02 -0300

    Improve Lograge configs (#873)

--- config/initializers/lograge.rb
+++ config/initializers/lograge.rb
@@ -2,14 +2,17 @@
 
 Rails.application.configure do
   config.lograge.enabled = true
+  config.lograge.base_controller_class = ['ActionController::API', 'ActionController::Base']
   config.lograge.custom_payload do |controller|
     request = controller.request
     {
+      current_user_id: controller.current_user&.id,
       host: request.host,
       path: request.path,
       query_string: request.query_string,
       referer: request.referer,
-      user_agent: request.user_agent
+      user_agent: request.user_agent,
+      remote_ip: request.remote_ip
     }
   end
 end
