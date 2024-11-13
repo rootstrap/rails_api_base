7ed2703b26f8f05affdf5950242f93cb3eeeb397
Santiago Bartesaghi <santib@hey.com>
2024-11-13 12:15:26 -0300

    Colorize logs in dev (#894)

--- config/initializers/lograge.rb
+++ config/initializers/lograge.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.configure do
-  config.lograge.enabled = true
+  config.lograge.enabled = Rails.env.production?
   config.lograge.base_controller_class = ['ActionController::API', 'ActionController::Base']
   config.lograge.custom_payload do |controller|
     request = controller.request
