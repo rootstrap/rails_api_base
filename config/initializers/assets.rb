c8732bf4576a8b36a1db989782f818afa399af53
Julian Pasquale <julianagustin78@gmail.com>
2024-11-25 23:35:57 -0300

    Use dartsass config

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -13,4 +13,8 @@ Rails.application.config.assets.paths << Rails.root.join('node_modules')
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in the app/assets
 # folder are already added.
-Rails.application.config.assets.precompile += %w[active_admin.js active_admin.css]
+Rails.application.config.assets.precompile += %w[active_admin.js]
+
+Rails.application.config.dartsass.builds = {
+  'active_admin.scss' => 'active_admin.css'
+}
