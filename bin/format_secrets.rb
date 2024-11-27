b9d75fc02b5e44dcdc424f8bb679374c8db7ba8f
Samir Tapiero <samir.tapiero@rootstrap.com>
2024-11-27 17:15:47 -0500

    Use describe-parameters to get all parameters, refactor in formater to define valid names

--- bin/format_secrets.rb
+++ bin/format_secrets.rb
@@ -3,6 +3,9 @@ require 'json'
 
 data = JSON.parse(ENV['SSM_PARAMETERS_JSON'])
 
-formatted_secrets = data.map { |param| "#{param["Name"]}=#{param["ARN"]}" }.join("\n")
+formatted_secrets = data.map do |param|
+  param_name = param["Name"].split('/').last.upcase
+"#{param_name}=#{param["ARN"]}"
+end.join("\n")
 
 puts formatted_secrets
