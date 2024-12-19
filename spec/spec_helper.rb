6884b70c33f3ee731abf97741d5492c1de675274
Santiago Bartesaghi <sbartesaghi@hotmail.com>
2024-12-19 13:04:46 -0300

    General improvements

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -57,7 +57,8 @@ RSpec.configure do |config|
   end
 
   config.shared_context_metadata_behavior = :apply_to_host_groups
-  config.order = 'random'
+  config.order = :random
+  Kernel.srand config.seed
   config.expect_with :rspec do |c|
     c.syntax = :expect
   end
