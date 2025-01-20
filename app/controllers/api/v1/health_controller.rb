393cc5513e271f1312412e79721185e24e7a8a0a
Ignacio Perez <ignacio.perez@rootstrap.com>
2025-01-20 16:42:16 -0300

    Another test change

--- app/controllers/api/v1/health_controller.rb
+++ app/controllers/api/v1/health_controller.rb
@@ -7,7 +7,7 @@ module API
       skip_after_action :verify_authorized
 
       def status
-        render json: { online: true }
+        render json: { online: true, test: true }
       end
     end
   end
