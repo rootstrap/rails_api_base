1bbe933bc002b5111506ee3b41ed22665866c880
Santiago Ramirez <santiago.ramirez@rootstrap.com>
2025-02-26 15:17:54 -0300

    fix comments

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -34,8 +34,6 @@ Rails.application.configure do
   # Store uploaded files on the local file system (see config/storage.yml for options).
   config.active_storage.service = :local
 
-  # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :letter_opener
 
