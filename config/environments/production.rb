db1f9cb034cd9d803c353b42a99d20eb8bd628d9
Santiago Ramirez <santiago.ramirez@rootstrap.com>
2025-02-26 18:35:57 -0300

    address comments

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -49,7 +49,7 @@ Rails.application.configure do
   config.log_level = ENV.fetch('RAILS_LOG_LEVEL', 'info')
 
   # Prevent health checks from clogging up the logs.
-  config.silence_healthcheck_path = '/up'
+  config.silence_healthcheck_path = '/api/v1/status'
 
   # Use a different cache store in production.
   # config.cache_store = :mem_cache_store
