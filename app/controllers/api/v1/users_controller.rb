c58982b7e4d226109bbb4b25e6f1e297bf14cec1
Gautham Nambi <gautham.nambi@freshworks.com>
2025-03-06 03:05:26 +0530

    Update users_controller.rb

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -16,7 +16,7 @@ module API
       private
 
       def update_user_params
-        params.expect(user: %i[username first_name last_name email])
+        params.expect(user: %i[first_name last_name email])
       end
     end
   end
