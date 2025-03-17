4e46828b6775166251bb1ea274f213b44a1dbffb
Gautham Nambi <gautham.nambi@freshworks.com>
2025-03-18 05:29:55 +0530

    chore: Edited get_delayed_jobs endpoint

--- app/controllers/api/v1/health_controller.rb
+++ app/controllers/api/v1/health_controller.rb
@@ -7,7 +7,10 @@ module API
       skip_after_action :verify_authorized
 
       def get_delayed_jobs
-        render json: { count: Delayed::Job.count }
+        total_count = Delayed::Job.count
+        if total_count > 0
+          return render json: { count: total_count, oldest: Delayed::Job.order(:created_at).first.created_at }
+        render json: { msg: 'No delayed jobs found' }
       end
 
       def status
