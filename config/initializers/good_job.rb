d164f82471a1cb7ffda1a20c989f78ef4483a422
Santiago Bartesaghi <sbartesaghi@hotmail.com>
2025-04-03 15:04:34 -0300

    Don't mix jobs enqueuing as part of transactions

--- config/initializers/good_job.rb
+++ config/initializers/good_job.rb
@@ -1,7 +1,8 @@
 # frozen_string_literal: true
 
-# Rails.application.configure do
-#   config.good_job.enable_cron = true
-#   config.good_job.cron_graceful_restart_period = 5.minutes
-#   config.good_job.cron = { example: { cron: '0 * * * *', class: 'ExampleJob'  } }
-# end
+Rails.application.configure do
+  config.good_job.enqueue_after_transaction_commit = true
+#  config.good_job.enable_cron = true
+#  config.good_job.cron_graceful_restart_period = 5.minutes
+#  config.good_job.cron = { example: { cron: '0 * * * *', class: 'ExampleJob'  } }
+end
