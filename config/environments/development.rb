7cd4018db2db135cdf42cf0a56d92b0fb7519379
Santiago Bartesaghi <sbartesaghi@hotmail.com>
2025-04-03 15:03:16 -0300

    Refactor

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -5,7 +5,6 @@ require 'active_support/core_ext/integer/time'
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
-  config.active_job.queue_adapter = :good_job
   # Make code changes take effect immediately without server restart.
   config.enable_reloading = true
 
@@ -63,6 +62,9 @@ Rails.application.configure do
 
   # Append comments with runtime information tags to SQL queries in logs.
   config.active_record.query_log_tags_enabled = true
+
+  # Use GoodJob adapter for ActiveJob
+  config.active_job.queue_adapter = :good_job
 
   # Highlight code that enqueued background job in logs.
   config.active_job.verbose_enqueue_logs = true
