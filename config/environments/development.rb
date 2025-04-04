90bf2892191aa26a2e17dbc455408db51d3662d1
Santiago Bartesaghi <santib@hey.com>
2025-04-04 14:28:50 -0300

    Replace DelayedJob with GoodJob (#1025)

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -63,6 +63,9 @@ Rails.application.configure do
   # Append comments with runtime information tags to SQL queries in logs.
   config.active_record.query_log_tags_enabled = true
 
+  # Use GoodJob adapter for ActiveJob
+  config.active_job.queue_adapter = :good_job
+
   # Highlight code that enqueued background job in logs.
   config.active_job.verbose_enqueue_logs = true
 
