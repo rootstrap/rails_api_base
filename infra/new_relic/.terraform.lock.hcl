328d4c4f93af3cb4cfe757b686fdbc154074acf6
Julian Pasquale <julianagustin78@gmail.com>
2025-05-06 13:00:49 -0300

    WIP

---
+++ infra/new_relic/.terraform.lock.hcl
@@ -0,0 +1,27 @@
+# This file is maintained automatically by "terraform init".
+# Manual edits may be lost in future updates.
+
+provider "registry.terraform.io/newrelic/newrelic" {
+  version = "3.61.0"
+  hashes = [
+    "h1:hvVjaulLrMGWjyJbFmlWoLFNO//espYTfUamuOWU5VY=",
+    "zh:0988ea702449dfe50f1a1d5b648a6d21e705ed96a79494526398e63a14b5cb49",
+    "zh:0fbf18f31ba85e3b6e440f19eb64e177811ffc833c68f99bf698a3285fe78d9f",
+    "zh:1b1d7e5dd63754604c6d4aa1494968bb9f74f594ad2a4e6391a170922ef5870f",
+    "zh:3daa7f87674f1159a020c1b15014990cab1a0331c0acc9b5cb4176c151ea5977",
+    "zh:55e18268e3f0233ac9905c65d8404708336edbead6d795c5e6481fd77d839788",
+    "zh:567a0204309a6de9d27ce2426ce498516604cfe947b923724a72b6f8b0c5c1c2",
+    "zh:5b56fdb5837e01e8701bf7888dd59a222b75a9926e05801054e0f5324a972276",
+    "zh:5d51077e392080cad2f981999c26ad62f242ed37b3fbedc0e57501b55c3e8b99",
+    "zh:7612219e2f9d62179b2b579d091f1c43c4aecfd0572d2be89cf9c762daf2d095",
+    "zh:7a7dcf55bfcc379ae0667a4181a90fdcbaef3cbf80b1f60d8770ecbdc99a5eee",
+    "zh:7e3565d8d4ed59f414d46c7beb05bdcdad307736fc77c9030af70e9c4658dbb0",
+    "zh:87f01c33ac82c6fea84aaae1c6aadb7f69931b3b10e7642f70442163e0bd0e3a",
+    "zh:8d8cfa78278ad5dcae90d5a16450e6cf73138028852647a33ecb9a2915aff483",
+    "zh:9cb4027e1099c70428e3b0cdd8c64e0316cf6967ef7e436c4fd45ed2cee46407",
+    "zh:ac0ba9be2fba2c61aac29b0ee8b766ce5f7ecbabd06ebcf558eb718d7a0f996e",
+    "zh:ced2c73d3d63a540bf7e4634996e9fa22e9c99dd13b87fe369d0da169c46fb3f",
+    "zh:df27dc9d839ee0279afca8c2594a72e301c639e7d18a0eaad81dda177bcf9289",
+    "zh:fbd1fee2c9df3aa19cf8851ce134dea6e45ea01cb85695c1726670c285797e25",
+  ]
+}
--- infra/new_relic/alerts.tf
+++ infra/new_relic/alerts.tf
@@ -13,15 +13,35 @@ variable "newrelic_region" {
 
 variable "app_name" {
   type = string
+  default = "rails_api_base"
 }
 
 # TODO:
 # Add names for different environments
 
+terraform {
+  # Require Terraform version 1.0 (recommended)
+  required_version = "~> 1.0"
+
+  # Require the latest 2.x version of the New Relic provider
+  required_providers {
+    newrelic = {
+      source  = "newrelic/newrelic"
+    }
+  }
+}
+
+# https://registry.terraform.io/providers/Newrelic/Newrelic/latest/docs/guides/migration_guide_alert_conditions
 provider "newrelic" {
   account_id = var.newrelic_account_id
   api_key    = var.newrelic_api_key
   region     = var.newrelic_region
+}
+
+data "newrelic_entity" "rails_api_base" {
+  name = "rails_api_base" # Must be an exact match to your application name in New Relic
+  domain = "APM" # or BROWSER, INFRA, MOBILE, SYNTH, depending on your entity's domain
+  type = "APPLICATION"
 }
 
 # Define your alert policy
@@ -29,6 +49,58 @@ resource "newrelic_alert_policy" "golden_metrics_policy" {
   name = "Golden Metrics Alert Policy"
 }
 
+resource "newrelic_alert_policy" "golden_signal_policy" {
+  name = "Golden Signals - ${data.newrelic_entity.rails_api_base.name}"
+}
+
+# Response time - Create Alert Condition
+resource "newrelic_nrql_alert_condition" "response_time_alert" {
+  policy_id = newrelic_alert_policy.golden_signal_policy.id
+  type = "static"
+  name = "Response Time - ${data.newrelic_entity.rails_api_base.name}"
+  description = "High Transaction Response Time"
+  # runbook_url = "https://www.example.com"
+  enabled = true
+  violation_time_limit_seconds = 3600
+
+  nrql {
+    query = "SELECT filter(average(newrelic.timeslice.value), WHERE metricTimesliceName = 'HttpDispatcher') OR 0 FROM Metric WHERE appId IN (${data.newrelic_entity.rails_api_base.application_id}) AND metricTimesliceName IN ('HttpDispatcher', 'Agent/MetricsReported/count')"
+  }
+
+  critical {
+    operator              = "above"
+    threshold             = 5
+    threshold_duration    = 300
+    threshold_occurrences = "ALL"
+  }
+}
+
+# Throughput condition
+resource "newrelic_nrql_alert_condition" "low_throughput" {
+  policy_id = newrelic_alert_policy.golden_metrics_policy.id
+  name = "Low Throughput"
+  type = "static"
+
+  nrql {
+    query = "FROM Transaction SELECT rate(count(*), 1 minute) WHERE appName = '${var.app_name}'"
+  }
+
+  critical {
+    threshold = 1
+    threshold_duration = 300
+    threshold_occurrences = "ALL"
+    operator = "below"
+  }
+
+  # signal {
+  #   aggregation_window = 60
+  #   aggregation_method = "event_flow"
+  # }
+
+  enabled = true
+}
+
+# Error percentage
 # Error rate condition
 resource "newrelic_nrql_alert_condition" "error_rate" {
   policy_id = newrelic_alert_policy.golden_metrics_policy.id
@@ -39,96 +111,128 @@ resource "newrelic_nrql_alert_condition" "error_rate" {
     query = "FROM Transaction SELECT percentage(count(*), WHERE error IS true) AS 'error_rate' WHERE appName = '${var.app_name}'"
   }
 
-  terms {
-    threshold      = 5
+  critical {
+    threshold = 5
     threshold_duration = 300
     threshold_occurrences = "ALL"
-    operator       = "above"
-    priority       = "critical"
+    operator = "above"
   }
 
-  signal {
-    aggregation_window = 60
-    aggregation_method = "event_flow"
-  }
+  # signal {
+  #   aggregation_window = 60
+  #   aggregation_method = "event_flow"
+  # }
 
   enabled = true
 }
 
-# Response time condition
-resource "newrelic_nrql_alert_condition" "response_time" {
-  policy_id = newrelic_alert_policy.golden_metrics_policy.id
-  name      = "High Response Time"
-  type      = "static"
+#######################################
+#          NOTIFICATIONS              #
+#######################################
 
-  nrql {
-    query = "FROM Transaction SELECT average(duration) WHERE appName = '${var.app_name}'"
+resource "newrelic_notification_destination" "team_email_destination" {
+  name = "email-notification"
+  type = "EMAIL"
+
+  property {
+    key = "email"
+    value = "julian.pasquale@rootstrap.com,ignacio.perez@rootstrap.com"
   }
-
-  terms {
-    threshold      = 1.5
-    threshold_duration = 300
-    threshold_occurrences = "ALL"
-    operator       = "above"
-    priority       = "critical"
-  }
-
-  signal {
-    aggregation_window = 60
-    aggregation_method = "event_flow"
-  }
-
-  enabled = true
 }
 
-# Throughput condition
-resource "newrelic_nrql_alert_condition" "low_throughput" {
-  policy_id = newrelic_alert_policy.golden_metrics_policy.id
-  name      = "Low Throughput"
-  type      = "static"
+resource "newrelic_notification_channel" "team_email_channel" {
+  name = "email-example"
+  type = "EMAIL"
+  destination_id = newrelic_notification_destination.team_email_destination.id
+  product = "IINT"
 
-  nrql {
-    query = "FROM Transaction SELECT rate(count(*), 1 minute) WHERE appName = '${var.app_name}'"
+  property {
+    key = "subject"
+    value = "New Alert"
   }
-
-  terms {
-    threshold      = 1
-    threshold_duration = 300
-    threshold_occurrences = "ALL"
-    operator       = "below"
-    priority       = "warning"
-  }
-
-  signal {
-    aggregation_window = 60
-    aggregation_method = "event_flow"
-  }
-
-  enabled = true
 }
 
-# Apdex condition
-resource "newrelic_nrql_alert_condition" "apdex" {
-  policy_id = newrelic_alert_policy.golden_metrics_policy.id
-  name      = "Low Apdex Score"
-  type      = "static"
-
-  nrql {
-    query = "FROM Transaction SELECT apdex(duration, t:0.5) WHERE appName = '${var.app_name}'"
-  }
-
-  terms {
-    threshold      = 0.85
-    threshold_duration = 300
-    threshold_occurrences = "ALL"
-    operator       = "below"
-    priority       = "critical"
-  }
-
-  signal {
-    aggregation_window = 60
-    aggregation_method = "event_flow"
-  }
-
+resource "newrelic_workflow" "team_workflow" {
+  name = "workflow-example"
+  enrichments_enabled = true
+  # destinations_enabled = true
   enabled = true
+  muting_rules_handling = "NOTIFY_ALL_ISSUES"
+
+  enrichments {
+    nrql {
+      name = "Log"
+      configuration {
+       query = "SELECT count(*) FROM Metric"
+      }
+    }
+  }
+
+  issues_filter {
+    name = "filter-example"
+    type = "FILTER"
+
+    predicate {
+      attribute = "accumulations.sources"
+      operator = "EXACTLY_MATCHES"
+      values = [ "newrelic" ]
+    }
+  }
+
+  destination {
+    channel_id = newrelic_notification_channel.team_email_channel.id
+  }
 }
+
+
+# # Response time condition
+# resource "newrelic_nrql_alert_condition" "response_time" {
+#   policy_id = newrelic_alert_policy.golden_metrics_policy.id
+#   name      = "High Response Time"
+#   type      = "static"
+
+#   nrql {
+#     query = "FROM Transaction SELECT average(duration) WHERE appName = '${var.app_name}'"
+#   }
+
+#   terms {
+#     threshold      = 1.5
+#     threshold_duration = 300
+#     threshold_occurrences = "ALL"
+#     operator       = "above"
+#     priority       = "critical"
+#   }
+
+#   signal {
+#     aggregation_window = 60
+#     aggregation_method = "event_flow"
+#   }
+
+#   enabled = true
+# }
+
+# # Apdex condition
+# resource "newrelic_nrql_alert_condition" "apdex" {
+#   policy_id = newrelic_alert_policy.golden_metrics_policy.id
+#   name      = "Low Apdex Score"
+#   type      = "static"
+
+#   nrql {
+#     query = "FROM Transaction SELECT apdex(duration, t:0.5) WHERE appName = '${var.app_name}'"
+#   }
+
+#   terms {
+#     threshold      = 0.85
+#     threshold_duration = 300
+#     threshold_occurrences = "ALL"
+#     operator       = "below"
+#     priority       = "critical"
+#   }
+
+#   signal {
+#     aggregation_window = 60
+#     aggregation_method = "event_flow"
+#   }
+
+#   enabled = true
+# }
