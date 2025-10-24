9571261e9e56836bd0995a14f1ce1c284ebcdb2c
Santiago Bartesaghi <sbartesaghi@hotmail.com>
2025-10-24 09:38:37 -0300

    Fix Sonar 2

--- app/models/concerns/ransackable.rb
+++ app/models/concerns/ransackable.rb
@@ -5,15 +5,19 @@ module Ransackable
 
   class_methods do
     def ransackable_attributes(auth_object = nil)
-      return column_names + ransackers.keys if auth_object == :admin
-
-      const_defined?(:RANSACK_ATTRIBUTES) ? self::RANSACK_ATTRIBUTES : []
+      if auth_object == :admin
+        column_names + ransackers.keys
+      else
+        const_defined?(:RANSACK_ATTRIBUTES) ? self::RANSACK_ATTRIBUTES : []
+      end
     end
 
     def ransackable_associations(auth_object = nil)
-      return reflect_on_all_associations.map { |association| association.name.to_s } if auth_object == :admin
-
-      const_defined?(:RANSACK_ASSOCIATIONS) ? self::RANSACK_ASSOCIATIONS : []
+      if auth_object == :admin
+        reflect_on_all_associations.map { |association| association.name.to_s }
+      else
+        const_defined?(:RANSACK_ASSOCIATIONS) ? self::RANSACK_ASSOCIATIONS : []
+      end
     end
   end
 end
