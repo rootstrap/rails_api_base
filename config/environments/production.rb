7f563943024eb0f6b483103b4ab613e139d41a39
Andres Garcia <andres@rootstrap.com>
2026-01-27 12:40:05 -0300

    Run app:update task

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -40,20 +40,19 @@ Rails.application.configure do
   # Skip http-to-https redirect for the default health check endpoint.
   # config.ssl_options = { redirect: { exclude: ->(request) { request.path == '/up' } } }
 
-  # Log to STDOUT by default
+  # Log to STDOUT with the current request id as a default log tag.
+  config.log_tags = [:request_id]
   config.logger = ActiveSupport::Logger.new(STDOUT)
                                        .tap  { |logger| logger.formatter = Logger::Formatter.new }
                                        .then { |logger| ActiveSupport::TaggedLogging.new(logger) }
 
-  # Prepend all log lines with the following tags.
-  config.log_tags = [:request_id]
   # Change to 'debug' to log everything (including potentially personally-identifiable information!)
   config.log_level = ENV.fetch('RAILS_LOG_LEVEL', 'info')
 
   # Prevent health checks from clogging up the logs.
   config.silence_healthcheck_path = '/api/v1/status'
 
-  # Use a different cache store in production.
+  # Replace the default in-process memory cache store with a durable alternative.
   # config.cache_store = :mem_cache_store
 
   # Use a real queuing backend for Active Job (and separate queues per environment).
