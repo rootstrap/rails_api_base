50c7608b085e12712ceea1851874f5ee008a11d7
Andres Garcia <andres@rootstrap.com>
2026-01-27 12:45:16 -0300

    Remove solid_queue from puma config

--- config/puma.rb
+++ config/puma.rb
@@ -49,9 +49,6 @@ environment ENV.fetch('RAILS_ENV') { 'development' }
 # Allow puma to be restarted by `bin/rails restart` command.
 plugin :tmp_restart
 
-# Run the Solid Queue supervisor inside of Puma for single-server deployments.
-plugin :solid_queue if ENV['SOLID_QUEUE_IN_PUMA']
-
 # Specify the PID file. Defaults to tmp/pids/server.pid in development.
 # In other environments, only set the PID file if requested.
 pidfile ENV['PIDFILE'] if ENV['PIDFILE']
